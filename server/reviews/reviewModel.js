444a66a2089d935f6c22c34203bbdc5d287d699c
Hughes89 <mik3h89@gmail.com>
2016-10-20 19:26:26 -0400

    Added package JSON, and built starting schemas

--- server/reviews/reviewModel.js
+++ server/reviews/reviewModel.js
@@ -1,1 +1,13 @@
 var mongoose = require('mongoose');
+
+var ReviewSchema = new mongoose.Schema({
+  username: String,
+  title: String,
+  date: String,
+  review: String,
+  rating: Number,
+  voteCount: Number
+});
+
+
+module.exports = mongoose.model('Review', ReviewSchema);
--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,1 +1,10 @@
 var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
