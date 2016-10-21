3aa5b2caea25b114b3cd6e7480e291b94b7d1fb9
Hughes89 <mik3h89@gmail.com>
2016-10-20 20:23:09 -0400

    Added delete review method

--- server/reviews/reviewController.js
+++ server/reviews/reviewController.js
@@ -2,6 +2,25 @@ var Review = require('../reviews/reviewModel.js');
 
 module.exports = {
   postReview: function (req, res, next) {
+    var data = req.body;
+    var review = new Review({
+      username: data.username,
+      movie: data.movie,
+      title: data.title,
+      date: new Date(),
+      review: data.review,
+      rating: data.rating,
+      voteCount: 0
+    });
+    review.save(function (err, revObj) {
+      if (err) {
+        console.log(err);
+        res.send(404);
+      } else {
+        console.log('Saved');
+        res.send(201);
+      }
+    });
   },
 
   getReviews: function (req, res, next) {
@@ -18,7 +37,16 @@ module.exports = {
   },
 
   deleteReview: function (req, res, next) {
-
+    var id = req.params.reviewId;
+    Review.find({ id: id })
+      .remove()
+      .exec(function (err, data) {
+        if (err) {
+          res.send(404);
+        } else {
+          res.send(200);
+        }
+      });
   },
 
   editReview: function (req, res, next) {
