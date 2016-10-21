3af339da0be2073fc9738fee0bcf071197a41727
Hughes89 <mik3h89@gmail.com>
2016-10-20 20:05:55 -0400

    addes post review method

--- server/reviews/reviewController.js
+++ server/reviews/reviewController.js
@@ -2,11 +2,19 @@ var Review = require('../reviews/reviewModel.js');
 
 module.exports = {
   postReview: function (req, res, next) {
-
   },
 
   getReviews: function (req, res, next) {
-
+    var id = req.params.id;
+    Review.find({movie: id})
+      .sort({date: -1})
+      .exec(function(err, review) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(review);
+        }
+      });
   },
 
   deleteReview: function (req, res, next) {
