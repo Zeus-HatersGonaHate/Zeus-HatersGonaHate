ed6e49ad2fe3b2577d84277bf90047f87163c517
Alex Leo <aleo1337@gmail.com>
2016-10-24 15:36:57 -0400

    Add fetch methods for API cals to themoviedb for popular, upcoming, latest

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -1,5 +1,26 @@
 angular.module('zeus.landing', [])
-  .controller('LandingController', function() {
+  .controller('LandingController', function($scope, Landing) {
+    $scope.populars = {};
+    $scope.latests = {};
+    $scope.upcomings = {};
 
+    $scope.fetchPopular = function() {
+      Landing.getPopular()
+        .then(function(data) {
+          $scope.populars = data.results;
+        });
+    };
+    $scope.fetchLatest = function() {
+      Landing.getLatest()
+        .then(function(data) {
+          $scope.latests = data.results;
+        });
+    };
+    $scope.fetchUpcoming = function() {
+      Landing.getUpcoming()
+        .then(function(data) {
+          $scope.upcomings = data.results;
+        });
+    };
 
   });
