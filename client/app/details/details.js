6eb1e90c91d1355958d1276e0dc8e54e8640c3b8
Nancy Du <nancyduyi@gmail.com>
2016-10-25 00:19:12 -0400

    Reviews now rerender after new review has been posted

--- client/app/details/details.js
+++ client/app/details/details.js
@@ -12,24 +12,29 @@ angular.module('zeus.details', [])
       $scope.poster_path = $scope.data.poster_path;
       $scope.overview = $scope.data.overview;
     });
-    Details.getReviews($scope.type, $scope.id).then(function (reviews) {
-      $scope.reviews = reviews.data;
-      if ($scope.reviews.length > 0) {
-        $scope.hasReview = true;
-      }
-    });
+    var getReviews = function(){
+      Details.getReviews($scope.type, $scope.id).then(function (reviews) {
+        $scope.reviews = reviews.data;
+        if ($scope.reviews.length > 0) {
+          $scope.hasReview = true;
+        }
+        return reviews.data;
+      });
+
+    }
+    getReviews();
+
     $scope.post = function(){
       var info = {
         username: 'Nancy', //Fix: need to add in real username
         title: $scope.reviewTitle,
         content: $scope.reviewBody,
         rating: $scope.reviewRating
-      }
-      Details.postReview($scope.type, $scope.id, info)
-
-      //prepend new review
-    }
-
+      };
+      Details.postReview($scope.type, $scope.id, info).then(function(review){
+        $scope.reviews.unshift(review.data);
+      })
+    };
   });
 
 
