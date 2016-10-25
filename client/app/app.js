dc314a90af35559b7bb18f29d3da2bed482ba308
Alex Leo <aleo1337@gmail.com>
2016-10-25 14:11:54 -0400

    Edit results url in router to include page number

--- client/app/app.js
+++ client/app/app.js
@@ -11,7 +11,7 @@ angular.module('zeus', [
     if (search.length < 1) {
       return;
     }
-    $location.path('/results/' + search);
+    $location.path('/results/' + search + '/1');
     $scope.searchQuery = '';
   };
 })
@@ -21,7 +21,7 @@ angular.module('zeus', [
       templateUrl: 'app/landing/landing.html',
       controller: 'LandingController'
     })
-    .when('/results/:search', {
+    .when('/results/:search/:page', {
       templateUrl: 'app/results/results.html',
       controller: 'ResultsController'
     })
