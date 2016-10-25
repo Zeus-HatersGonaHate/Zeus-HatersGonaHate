720326f91ab7d12fb1e435606961f79fcbcc192f
Alex Leo <aleo1337@gmail.com>
2016-10-25 14:13:25 -0400

    Save current movie being search on model and make totalPages array

--- client/app/results/results.js
+++ client/app/results/results.js
@@ -2,13 +2,13 @@ angular.module('zeus.results', [])
 .controller('ResultsController', function($scope, Results, $routeParams) {
   $scope.results = [];
   $scope.loaded = false;
-  $scope.pages = 1;
-  var search = $routeParams.search;
-  Results.multiSearch(search)
+  $scope.search = $routeParams.search;
+  var page = $routeParams.page || 1;
+  Results.multiSearch($scope.search, page)
     .then(function(results) {
       $scope.loaded = true;
       $scope.results = results.data.results;
-      $scope.totalPages = results.data.total_pages;
+      $scope.totalPages = _.range(1, results.data.total_pages+1);
       $scope.totalResults = results.data.total_results;
       console.log($scope.results);
     });
