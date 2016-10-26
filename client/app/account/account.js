6987302f8438e8e0c82bd546d6b1e5ba113ef418
Luke Wilson <wilson.palooza@gmail.com>
2016-10-26 11:34:54 -0700

    - Adds necessary $scope variables - Adds getAccountReviews, getAccountRecentlyViewed, and getAccountFavorites methods

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -1,4 +1,33 @@
 angular.module('zeus.account', [])
-  .controller('AccountController', function($scope) {
+  .controller('AccountController', function($scope, Account) {
     $scope.test = "--- text from AccountController";
+    $scope.username = "hardcoded_username";
+    $scope.email = "hardcoded@email.com";
+    $scope.reviews = {}; //or [];
+    $scope.recentlyViewed = {}; //or [];
+    $scope.favorites = {}; // or [];
+
+    var getAccountReviews = function () {
+      Account.getAccountReviews($scope.username) //username will change, so probably better to pass user id here
+        .then(function(reviews) {
+          $scope.reviews = reviews.data;
+        });
+    };
+    getAccountReviews();
+
+    var getAccountRecentlyViewed = function() {
+      Account.getAccountRecentlyViewed($scope.username)
+        .then(function(recentlyViewed) {
+          $scope.recentlyViewed = recentlyViewed.data;
+        });
+    };
+    getAccountRecentlyViewed();
+
+    var getAccountFavorites = function() {
+      Account.getAccountFavorites($scope.username)
+        .then(function(favorites) {
+          $scope.favorites = favorites.data;
+        });
+    };
+    getAccountFavorites();
   });
