68acf2f2eb874640a61b700aec7c2f2259f02d28
Luke Wilson <wilson.palooza@gmail.com>
2016-10-27 07:08:30 -0700

    - Creates UserController for user.html

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -1,11 +1,29 @@
 angular.module('zeus.user', [])
-  .controller('UserController', function($scope, User) {
-    $scope.data = {};
-    $scope.data.username = '';
+  .controller('UserController', function($scope, User, $routeParams) {
+    $scope.username = "wilson.palooza4";
+    // $scope.userId = User.getUserId($scope.username)
 
-    $scope.updateProfile = function () {
-      User.editUser($scope.data);
+    $scope.profilePic = "https://lh3.googleusercontent.com/-kKTmPYr4dZI/AAAAAAAAAAI/AAAAAAAAAMw/D7Ak_C3TSWY/photo.jpg";
+    $scope.email = "hardcoded@email.com";
+    $scope.reviews = {}; //or [];
+    $scope.favorites = {}; // or [];
+
+    var getUserReviews = function () {
+      User.getUserReviews($scope.username) //username will change, so probably better to pass user id here
+        .then(function(reviews) {
+          console.log('------------------');
+          console.log(reviews);
+          $scope.reviews = reviews.data;
+
+        });
     };
+    getUserReviews();
 
-
+    var getUserFavorites = function() {
+      User.getUserFavorites($scope.username)
+        .then(function(favorites) {
+          $scope.favorites = favorites.data;
+        });
+    };
+    getUserFavorites();
   });
