127df0392fb0f87be4d5be98fcae57478e3f322d
Luke Wilson <wilson.palooza@gmail.com>
2016-10-27 08:49:10 -0700

    - Removes separate getUserReviews function and instead just calls the getUserReviews function from the User factory

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -2,15 +2,6 @@ angular.module('zeus.user', [])
   .controller('UserController', function($scope, User, $routeParams) {
     $scope.username = $routeParams.username;
 
-    var getUserReviews = function (userIdAuth) {
-      User.getUserReviews(userIdAuth) //username will change, so probably better to pass user id here
-        .then(function(reviews) {
-          console.log('-----reviews-----');
-          console.log(reviews);
-          return reviews;
-        });
-    };
-
     //set up user information based on username given in route
     User.getUserId($routeParams.username)
       .then(function(user){
@@ -20,6 +11,9 @@ angular.module('zeus.user', [])
         $scope.profilePic = userObj.profilePicLink;
         $scope.email = userObj.email;
         $scope.favorites = userObj.favorites;
-        $scope.reviews = getUserReviews($scope.userIdAuth);
+        User.getUserReviews($scope.userIdAuth)
+          .then(function(reviews) {
+            $scope.reviews = reviews;
+          });
       });
   });
