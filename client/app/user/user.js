9b5362d0ff98ce49192a3b4c572cc4a39f54f995
Luke Wilson <wilson.palooza@gmail.com>
2016-10-27 13:48:15 -0700

    - Adds directives - Adds default currentView value

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -1,20 +1,45 @@
 angular.module('zeus.user', [])
-  .controller('UserController', function($scope, User, $stateParams) {
-    $scope.username = $stateParams.username;
+.controller('UserController', function($scope, User, $stateParams) {
+  $scope.currentView = 'overview';
+  $scope.username = $stateParams.username;
 
-    //set up user information based on username given in route
-    User.getUserId($stateParams.username)
-      .then(function(user){
-        var userObj = user;
-        $scope.userId = userObj._id;
-        $scope.userIdAuth = userObj.user_id;
-        $scope.about = userObj.about;
-        $scope.profilePic = userObj.profilePicLink;
-        $scope.email = userObj.email;
-        $scope.favorites = userObj.favorites;
-        User.getUserReviews($scope.userIdAuth)
-          .then(function(reviews) {
-            $scope.reviews = reviews;
-          });
-      });
-  });
+  //set up user information based on username given in route
+  User.getUserId($stateParams.username)
+    .then(function(user) {
+      var userObj = user;
+      $scope.userId = userObj._id;
+      $scope.userIdAuth = userObj.user_id;
+      $scope.about = userObj.about;
+      $scope.profilePic = userObj.profilePicLink;
+      $scope.email = userObj.email;
+      $scope.favorites = userObj.favorites;
+      User.getUserReviews($scope.userIdAuth)
+        .then(function(reviews) {
+          $scope.reviews = reviews;
+        });
+    });
+})
+.directive('userOverview', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: true,
+    templateUrl: 'app/user/userOverview.html'
+  };
+})
+.directive('userReviews', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: true,
+    templateUrl: 'app/user/userReviews.html'
+  };
+})
+.directive('userFavorites', function() {
+  return {
+    restrict: 'AE',
+    replace: true,
+    scope: true,
+    templateUrl: 'app/user/userFavorites.html'
+  };
+});
