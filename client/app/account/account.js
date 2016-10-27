5cf3868ec330e68561133526afedcce473b5c509
Luke Wilson <wilson.palooza@gmail.com>
2016-10-27 07:10:10 -0700

    - Returns AccountController to barebones (content moved to user.js)

--- client/app/account/account.js
+++ client/app/account/account.js
@@ -1,24 +1,4 @@
 angular.module('zeus.account', [])
-  .controller('AccountController', function($scope, Account) {
-    $scope.profilePic = "https://lh3.googleusercontent.com/-kKTmPYr4dZI/AAAAAAAAAAI/AAAAAAAAAMw/D7Ak_C3TSWY/photo.jpg";
-    $scope.username = "wilson.palooza4";
-    $scope.email = "hardcoded@email.com";
-    $scope.reviews = {}; //or [];
-    $scope.favorites = {}; // or [];
+  .controller('AccountController', function($scope, Account, $routeParams) {
 
-    var getAccountReviews = function () {
-      Account.getAccountReviews($scope.username) //username will change, so probably better to pass user id here
-        .then(function(reviews) {
-          $scope.reviews = reviews.data;
-        });
-    };
-    getAccountReviews();
-
-    var getAccountFavorites = function() {
-      Account.getAccountFavorites($scope.username)
-        .then(function(favorites) {
-          $scope.favorites = favorites.data;
-        });
-    };
-    getAccountFavorites();
   });
