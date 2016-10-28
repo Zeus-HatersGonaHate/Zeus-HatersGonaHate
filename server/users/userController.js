146577db91f0957bf6cfd1281cbc1e9e7ef5a5a9
Hughes89 <mik3h89@gmail.com>
2016-10-28 13:18:56 -0400

    Added location and about to users

--- server/users/userController.js
+++ server/users/userController.js
@@ -19,6 +19,8 @@ module.exports = {
               email: info.email,
               user_id: info.user_id,
               username: username,
+              location: '',
+              about: '',
               profilePicLink: info.picture || 'https://www.drupal.org/files/issues/default-avatar.png',
               favourites: []
             });
