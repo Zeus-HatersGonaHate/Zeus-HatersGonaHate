e25e793d10c66e22e9b52b0626e47b1f011c6ba7
Alex Leo <aleo1337@gmail.com>
2016-10-30 22:05:03 -0400

    Add comments to reviews

--- client/app/reviews/reviews.js
+++ client/app/reviews/reviews.js
@@ -10,45 +10,45 @@ angular.module('zeus.reviews', [])
   ReviewsVm.movie = {};
   ReviewsVm.currentUser = JSON.parse(localStorage.getItem('profile'));
   Reviews.getReviewById(ReviewsVm.id)
-    .then(function(reviewInfo){
+    .then(function(reviewInfo) {
       ReviewsVm.review = reviewInfo.review;
       ReviewsVm.user = reviewInfo.user;
       //Find movie info for review
       Details.getDetails(reviewInfo.review.type, reviewInfo.review.typeId)
-      .then(function(movieInfo){
+      .then(function(movieInfo) {
         ReviewsVm.movie = movieInfo;
       });
       //Find comments for review
       Comment.getComment(ReviewsVm.id)
-      .then(function(comments){
+      .then(function(comments) {
         ReviewsVm.comments = comments.comments;
         ReviewsVm.commentsUser = comments.users;
         console.log(comments);
       });
     });
 
-  ReviewsVm.postComment = function(){
+  ReviewsVm.postComment = function() {
     var comment = {
       user_id: ReviewsVm.currentUser,
       review_id: ReviewsVm.id,
       content: ReviewsVm.content
-    }
+    };
     Comment.postComment(comment)
-    .then(function(newComment){
+    .then(function(newComment) {
       ReviewsVm.comments.unshift(newComment.comment);
-      ReviewsVm.commentsUser[newComment.user.user_id] = newComment.user
+      ReviewsVm.commentsUser[newComment.user.user_id] = newComment.user;
     });
     ReviewsVm.content = '';
-  }
+  };
 
-  ReviewsVm.deleteComment = function(comment){
+  ReviewsVm.deleteComment = function(comment) {
     Comment.deleteComment(comment._id);
     var index = ReviewsVm.comments.indexOf(comment);
     ReviewsVm.comments.splice(index, 1);
-  }
+  };
 
   ReviewsVm.vote = function(vote, auth) {
-    if(auth){
+    if (auth) {
       Reviews.upvote(ReviewsVm.review._id, vote)
         .then(function(reviewInfo) {
           ReviewsVm.review.voteCount = reviewInfo.voteCount;
@@ -59,14 +59,14 @@ angular.module('zeus.reviews', [])
     }
   };
 
-  ReviewsVm.edit = function(){
-    $location.path($location.url()+"/edit");
+  ReviewsVm.edit = function() {
+    $location.path($location.url() + '/edit');
   };
 
   ReviewsVm.delete = function() {
-      Reviews.deleteReview(ReviewsVm.review._id);
-      $location.path('/'); //Needs to redirect to somewhere useful
-    };
+    Reviews.deleteReview(ReviewsVm.review._id);
+    $location.path('/'); //Needs to redirect to somewhere useful
+  };
 
   ReviewsVm.login = authService.login;
 });
