64503a6d6a7fb0d4a1403e077ec37e4f622885b9
David Bisrat <dabisrat@gmail.com>
2016-11-03 17:18:05 -0400

    changed db name

--- server/server.js
+++ server/server.js
@@ -10,11 +10,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 
 //Your DB connection here
-<<<<<<< HEAD
-mongoose.connect('mongodb://localhost/GOD');
-=======
 mongoose.connect('mongodb://localhost/zeus');
->>>>>>> d16a70e1bd50c42f3f25a4cce361818d5f377ce1
 
 require('./config/routes.js')(app, express);
 
